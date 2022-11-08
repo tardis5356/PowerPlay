614a0cbdbfe9576569ffe6fe8ce842010cb82af4
thesimg <graham.simons05@gmail.com>
2022-11-07 22:04:01 -0500

    2022-11-07 position tuned with AXON SERVO FOR ARM!!!!!! don't break anything while I'm gone

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Junctions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Junctions.java
@@ -6,9 +6,9 @@ public enum Junctions {
     FULL_RETRACTION(0), // 0
     INTAKE(30), // 100
     GROUND_JUNCTION(100),
-    LOW_JUNCTION(300), // 200
-    MEDIUM_JUNCTION(550), // 450
-    HIGH_JUNCTION(780);// 600
+    LOW_JUNCTION(350), // 200
+    MEDIUM_JUNCTION(600), // 450
+    HIGH_JUNCTION(820);// 600
 
     public int position;
 
